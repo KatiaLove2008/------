2e76766f413a2c95800f69d1e55a68d15d04c6de
KatiaLove2008 <katia.love.pizza@gmail.com>
2025-05-29 21:02:59 +0300

    .......

---
+++ js/theme-toggle.js
@@ -0,0 +1,27 @@
+const toggleBtn = document.getElementById("theme-toggle");
+const body = document.body;
+
+function setTheme(theme) {
+  if (theme === "dark") {
+    body.classList.add("dark-theme");
+    toggleBtn.textContent = "Світла тема";
+  } else {
+    body.classList.remove("dark-theme");
+    toggleBtn.textContent = "Темна тема";
+  }
+  localStorage.setItem("theme", theme);
+}
+
+// При завантаженні сторінки
+document.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme") || "light";
+  setTheme(savedTheme);
+});
+
+toggleBtn.addEventListener("click", () => {
+  if (body.classList.contains("dark-theme")) {
+    setTheme("light");
+  } else {
+    setTheme("dark");
+  }
+});
